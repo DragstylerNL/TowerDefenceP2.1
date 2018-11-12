564149671803d9487b8812fdd18ab77ba8355176
DragstylerNL <dragstylernl@gmail.com>
2018-11-12 13:44:51 +0100

    normyturret and base start

---
+++ TowerDefence/Assets/Scripts/Turret/BaseTurret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTurret : MonoBehaviour {
+
+    private string TRname;
+    private float TRdamage, TRfirerate;
+	
+    public void SetInfo(string name, float dmg,float firerate)
+    {
+        TRname = name;
+        TRdamage = dmg;
+        TRfirerate = firerate;
+    }
+	void Start () {
+        GameController gmc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        gmc.GetSelectedPos();
+	}
+}
---
+++ TowerDefence/Assets/Scripts/Turret/NormyTurret.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormyTurret : BaseTurret {
+    
+
+	void Start () {
+        SetInfo("NormyTurret", 10f, 1f);
+	}
+	
+}
